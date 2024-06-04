70595967a7b8cdde7000f7dad34bb48f122de6ff
ana <anapaulac@live.com>
2024-06-04 17:49:26 -0300

    feat: send message

---
+++ README.md
@@ -0,0 +1,6 @@
+
+### conteudo do .env
+URL_Z_API=URL_Z_API=https://api.z-api.io/instances/{instances}/token/{token}
+
+### para execurar usando nodemon
+npm run dev
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+const { sendMessage } = require("./whatsapp-api/api");
+
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.use(bodyParser.json());
+
+app.post("/send-message", async (req, res) => {
+  console.log("request recived: /send-message");
+  const { phone, message } = req.body;
+
+  try {
+    const result = await sendMessage(phone, message);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao enviar mensagem" });
+  }
 });
 
 app.listen(port, () => {
